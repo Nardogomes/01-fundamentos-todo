5995976ae6172726e94bc3aec33f6305feffd57a
Ednardo Gomes <ednardo@gomes.com>
2022-08-03 16:56:10 -0300

    Criando a funcionalidade de criar lista de tarefas.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,31 +1,53 @@
-import { Trash, PlusCircle } from "phosphor-react";
+import { Trash, PlusCircle, ClipboardText } from "phosphor-react";
+import { ChangeEvent, useState } from "react";
 
 import styles from "./TodoList.module.css";
 
 export function TodoList() {
+  const [listTasks, setListTasks] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState('');
+
+  function handleCreateNewTask() {
+    setListTasks([...listTasks, newTask]);
+    setNewTask('');
+  }
+  
+  function handleNewTask(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.newTask}>
-        <input type="text" />
+        <input type="text" onChange={handleNewTask} value={newTask}/>
 
-        <button>
+        <button onClick={handleCreateNewTask}>
           Nova tarefa
           <PlusCircle size={32} />
         </button>
       </div>
 
       <div className={styles.content}>
-        <div className={styles.contentTask}>
-          <div className={styles.contentInfo} >
-            <input type="checkbox" name="task" />
-            
-            <p>Estudar Javascrit</p>
-          </div>
+        {listTasks.length !== 0 ? listTasks.map((task, index) => {
+          return (
+            <div key={index} className={styles.contentTask}>
+              <div className={styles.contentInfo} >
+                <input type="checkbox" name="task" />
+                
+                <p>{task}</p>
+              </div>
 
-          <button title="Deletar tarefa">
-            <Trash size={24} />
-          </button>
-        </div>
+              <button title="Deletar tarefa">
+                <Trash size={24} />
+              </button>
+            </div>
+          );
+        }) :
+          <div className={styles.contentTaskEmpty}>
+            <ClipboardText size={32} />
+            <p>{`Não há tarefas para serem realizdas.`}</p>
+          </div>
+        }
       </div>
     </div>
   );
